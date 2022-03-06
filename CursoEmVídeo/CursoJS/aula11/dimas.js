18435fdd1383fa0728cd55a842e40d328bd566ab
Dimas Capelari <97951537+DimasCapelari@users.noreply.github.com>
2022-03-06 17:55:45 -0300

    atualizações

---
+++ CursoEmVídeo/CursoJS/aula11/dimas.js
@@ -0,0 +1,5 @@
+var pais = 'USA'
+console.log(`Seu Pais de é o ${pais}`)
+if (pais != 'Brasil') {
+  console.log(`Você é Extrangeiro`)
+}
---
+++ CursoEmVídeo/CursoJS/aula11/ex009.js
@@ -0,0 +1,16 @@
+/*var pais = 'EUA'
+console.log(`Vivendo em ${pais}`)
+if (pais == 'Brasil') {
+  console.log('Você é Brasileiro!')
+} else {
+  console.log('Você é Estrangeiro!')
+}*/
+
+
+var pais = 'USA'
+console.log(`Vivendo em ${pais}`)
+if (pais != 'Brasil') {
+  console.log('Você é Estrangeiro!')
+} else {
+  console.log('Você é Brasileiro!')
+}
