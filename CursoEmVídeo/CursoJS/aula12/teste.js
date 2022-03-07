f172c3b557183c0df638aa0f8b5bad7b2995a1fd
Dimas Capelari <97951537+DimasCapelari@users.noreply.github.com>
2022-03-07 14:14:40 -0300

    atualizações

---
+++ CursoEmVídeo/CursoJS/aula12/teste.js
@@ -0,0 +1,11 @@
+var hora = 4
+console.log(`Agora são ${hora} horas.`)
+  if (hora <= 1 || hora < 5) {
+    console.log('Boa madrugada')
+} else if (hora < 12) {
+  console.log('Bom dia')
+} else if (hora <= 18){
+  console.log('Boa tarde')
+} else if (hora <= 23){
+  console.log('Boa noite')
+} 
