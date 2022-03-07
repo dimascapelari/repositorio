3b91d3bab7c22ec72f73746a04cef0b026199525
Dimas Capelari <97951537+DimasCapelari@users.noreply.github.com>
2022-03-07 08:25:41 -0300

    Update ex012.js

--- CursoEmVídeo/CursoJS/aula12/ex012.js
+++ CursoEmVídeo/CursoJS/aula12/ex012.js
@@ -1,2 +1,9 @@
-var hora = 8
+var hora = 20
 console.log(`Agora são exatamente ${hora} horas.`)
+if (hora < 12) {
+  console.log('Bom Dia!')
+} else if (hora <= 18) {
+  console.log('Boa Tarde!')
+} else {
+  console.log('Boa Noite!')
+}
