d9592ebbbdea66f7634f89d89e82bca883731c01
Dimas Capelari <97951537+DimasCapelari@users.noreply.github.com>
2022-03-08 16:46:04 -0300

    atualizações

--- projeto2/index.js
+++ projeto2/index.js
@@ -1,4 +1,6 @@
 function somar() {
+  var nome = document.getElementById('nomealuno')
+
   var tn1 = document.getElementById('txtn1')
   var tn2 = document.querySelector('#txtn2')
   var tn3 = document.getElementById('txtn3')
@@ -9,13 +11,15 @@ function somar() {
   var n3 = Number(tn3.value)
   var n4 = Number(tn4.value)
   var soma = n1 + n2 + n3 + n4
+  var nomealuno = (nome.value)
+  var media = soma / 4
   
-  res.innerHTML = `A média anual foi igual a <strong>${soma}</strong>`
+  res.innerHTML = `A média anual foi igual a <strong>${media}</strong>`
   final.innerHTML = `Média = <strong>${soma}</strong>`
-  if (soma >= 20) {
-    final.innerHTML = `<p style="color:#0000FF;">Você está <strong>APROVADO</strong>!</p>`
-  } else if (soma < 20) {
-    final.innerHTML = `<p style="color:#FF0000;">Você está <strong>REPROVADO!</p>`
+  if (soma >= 24) {
+    final.innerHTML = `<p style="color:#0000FF;">${nomealuno} está <strong>APROVADO</strong>!</p>`
+  } else if (soma < 24) {
+    final.innerHTML = `<p style="color:#FF0000;">${nomealuno} está <strong>REPROVADO!</p>`
   }
   
 }
