498b7cb9a4c6c865e48c7c013fd260264ae6ad17
Dimas Capelari <97951537+DimasCapelari@users.noreply.github.com>
2022-03-08 21:57:04 -0300

    atualizações

--- projeto2/index.js
+++ projeto2/index.js
@@ -1,6 +1,6 @@
 function somar() {
+  var mat = document.getElementById('materia')
   var nome = document.getElementById('nomealuno')
-
   var tn1 = document.getElementById('txtn1')
   var tn2 = document.querySelector('#txtn2')
   var tn3 = document.getElementById('txtn3')
@@ -11,15 +11,16 @@ function somar() {
   var n3 = Number(tn3.value)
   var n4 = Number(tn4.value)
   var soma = n1 + n2 + n3 + n4
+  var materia = (mat.value)
   var nomealuno = (nome.value)
   var media = soma / 4
   
-  res.innerHTML = `A média anual foi igual a <strong>${media}</strong>`
+  res.innerHTML = `A média anual foi <strong>${media}</strong>`
   final.innerHTML = `Média = <strong>${soma}</strong>`
   if (soma >= 24) {
-    final.innerHTML = `<p style="color:#0000FF;">${nomealuno} está <strong>APROVADO</strong>!</p>`
+    final.innerHTML = `<p style="color:#0000FF;">${nomealuno} está <strong>APROVADO</strong> em ${materia}!</p>`
   } else if (soma < 24) {
-    final.innerHTML = `<p style="color:#FF0000;">${nomealuno} está <strong>REPROVADO!</p>`
+    final.innerHTML = `<p style="color:#FF0000;">${nomealuno} está <strong>REPROVADO</strong> em ${materia}!</p>`
   }
   
 }
