65eaa28bc596b80b4604280deda8f1cf10341761
Dimas Capelari <97951537+DimasCapelari@users.noreply.github.com>
2022-03-12 16:56:24 -0300

    atualizações

--- CursoEmVídeo/CursoJS/aula15/ambiente.js
+++ CursoEmVídeo/CursoJS/aula15/ambiente.js
@@ -1,5 +1,13 @@
 let num = [5, 8, 2, 9, 3]
-
-  console.log(`Nosso vetor é o ${num}`)
+  num.push(1) //acrescenta + 1 número
+  num.sort() //organiza os números por ordem crescente
+  console.log(num)
   console.log(`O vetor tem ${num.length} posições`)
-  console.log(`O primeiro valor do vetor é ${num[1]}`)
+  console.log(`O primeiro valor do vetor é ${num[0]}`)
+  let pos = num.indexOf(8)
+  if (pos == -1){
+    console.log(`O valor não foi encontrado!`)
+  } else {
+    console.log(`O valor 8 está na posição ${pos}`)
+  }
+  
