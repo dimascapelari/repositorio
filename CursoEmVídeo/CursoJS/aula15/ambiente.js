ebb1f6bc4aed50ec86fc0df666cfae53df479661
Dimas Capelari <97951537+DimasCapelari@users.noreply.github.com>
2022-03-14 19:41:04 -0300

    Update ambiente.js

--- CursoEmVídeo/CursoJS/aula15/ambiente.js
+++ CursoEmVídeo/CursoJS/aula15/ambiente.js
@@ -4,7 +4,7 @@ let num = [5, 8, 2, 9, 3]
   console.log(num)
   console.log(`O vetor tem ${num.length} posições`)
   console.log(`O primeiro valor do vetor é ${num[0]}`)
-  let pos = num.indexOf(8)
+  let pos = num.indexOf(7)
   if (pos == -1){
     console.log(`O valor não foi encontrado!`)
   } else {
