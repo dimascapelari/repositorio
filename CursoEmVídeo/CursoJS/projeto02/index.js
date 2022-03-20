18fc44e53dc53990acbe221ae30fb9ba17b4d147
Dimas Capelari <97951537+DimasCapelari@users.noreply.github.com>
2022-03-20 13:28:11 -0300

    atualizações

--- CursoEmVídeo/CursoJS/projeto02/index.js
+++ CursoEmVídeo/CursoJS/projeto02/index.js
@@ -5,17 +5,22 @@ function carregar() {
   var data = new Date()
   var hora = data.getHours()
   var min = data.getMinutes()
-  //var hora = 19
+  //var hora = 2
   msg.innerHTML = `Agora são ${hora} horas e ${min} minutos`
-  if (hora >= 0 && hora < 12) {
+
+  if (hora <= 1 || hora < 6) {
+    img.src = './img/madrugada.png'
+    document.body.style.background = 'black'
+    sauda.innerHTML = `Boa madrugada!`
+  } else if (hora < 12) {
     img.src = './img/manha.png'
     document.body.style.background = '#FCB333'
     sauda.innerHTML = `Bom dia!`
-  } else if (hora >= 12 && hora <= 18) {
+  } else if (hora <= 17) {
     img.src = './img/tarde.png'
     document.body.style.background = '#D89B7F'
     sauda.innerHTML = `Boa tarde!`
-  } else {
+  } else if (hora <= 23) {
     img.src = './img/noite.png'
     document.body.style.background = '#193954'
     sauda.innerHTML = `Boa noite!`
