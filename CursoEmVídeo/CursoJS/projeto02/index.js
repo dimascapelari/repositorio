b7aa6e33a7363de5e2171f1bb76706da75a36aa2
Dimas Capelari <97951537+DimasCapelari@users.noreply.github.com>
2022-03-20 11:11:24 -0300

    atualizações

--- CursoEmVídeo/CursoJS/projeto02/index.js
+++ CursoEmVídeo/CursoJS/projeto02/index.js
@@ -1,1 +1,18 @@
-alert('olá')
+function carregar() {
+  var msg = document.getElementById('msg')
+  var img = document.getElementById('imagem')
+  var data = new Date()
+  var hora = data.getHours()
+  //var hora = 11
+  msg.innerHTML = `Agora são ${hora} horas.`
+  if (hora >= 0 && hora < 12) {
+    img.src = './img/manha.png'
+    document.body.style.background = '#FCB333'
+  } else if (hora >= 12 && hora <= 18) {
+    img.src = './img/tarde.png'
+    document.body.style.background = '#D89B7F'
+  } else {
+    img.src = './img/noite.png'
+    document.body.style.background = '#193954'
+  }
+}
