68ba2c6b5f3af16b4c484c17f12d3510289a4bdf
Dimas Capelari <97951537+DimasCapelari@users.noreply.github.com>
2022-04-01 21:13:11 -0300

    atualização

--- CursoEmVídeo/CursoJS/projeto03/index.js
+++ CursoEmVídeo/CursoJS/projeto03/index.js
@@ -1,5 +1,21 @@
 function verificar() {
   var data = new Date()
   var ano = data.getFullYear()
-
+  var fano = document.getElementById('txtano')
+  var res = document.getElementById('res')
+  if (fano.value.length == 0 || Number(fano.value) > ano) {
+    alert('[ERRO] Verifique os dados e tente novamente!')
+  } else {
+    var fsex = document.getElementsByName('radsex')
+    var idade = ano - Number(fano.value)
+    //res.innerHTML = `Idade calculada: ${idade}`
+    var genero = ''
+    if (fsex[0].checked) {
+      genero = 'Homem'
+    } else if (fsex[1].checked) {
+      genero = 'Mulher'
+    }
+    res.style.textAlign = 'center'
+    res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+  }
 }
