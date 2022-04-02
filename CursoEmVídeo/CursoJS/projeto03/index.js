ea7516a1dfc557997bfd18976e3b83894dd03785
Dimas Capelari <97951537+DimasCapelari@users.noreply.github.com>
2022-04-02 08:35:06 -0300

    atualização

--- CursoEmVídeo/CursoJS/projeto03/index.js
+++ CursoEmVídeo/CursoJS/projeto03/index.js
@@ -8,14 +8,42 @@ function verificar() {
   } else {
     var fsex = document.getElementsByName('radsex')
     var idade = ano - Number(fano.value)
-    //res.innerHTML = `Idade calculada: ${idade}`
     var genero = ''
+    var img = document.createElement('img')
+    img.setAttribute('id', 'foto')
     if (fsex[0].checked) {
       genero = 'Homem'
+      if (idade >= 0 && idade < 10) {
+        // Criança
+        img.setAttribute('src', './img/bebe-homem.png')
+      } else if (idade < 21) {
+        // Jovem
+        img.setAttribute('src', './img/homem-jovem.png')
+      } else if (idade < 50) {
+        // Adulto
+        img.setAttribute('src', './img/homem-adulto.png')
+      } else {
+        // Idoso
+        img.setAttribute('src', './img/homem-idoso.png')
+      }
     } else if (fsex[1].checked) {
       genero = 'Mulher'
+      if (idade >= 0 && idade < 10) {
+        // Criança
+        img.setAttribute('src', './img/bebe-mulher.png')
+      } else if (idade < 21) {
+        // Jovem
+        img.setAttribute('src', './img/mulher-jovem.png')
+      } else if (idade < 50) {
+        // Adulto
+        img.setAttribute('src', './img/mulher-adulta.png')
+      } else {
+        // Idoso
+        img.setAttribute('src', './img/mulher-idosa.png')
+      }
     }
     res.style.textAlign = 'center'
     res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+    res.appendChild(img)
   }
 }
