9dcf17c7ebec3d43034407509d9b5c9e1525fa08
Dimas Capelari <97951537+DimasCapelari@users.noreply.github.com>
2022-04-05 20:28:30 -0300

    dia03

---
+++ 7DaysOfCode/dia03/script.js
@@ -0,0 +1,29 @@
+const area = prompt("Você quer seguir para área de 'Front-End' ou de 'Back-End'? Digite o nome da área:");
+let linguagem = "";
+if (area === "Front-End"){
+    linguagem = prompt("Você quer aprender React ou Vue?");
+}
+else if (area === "Back-End"){
+    linguagem = prompt("Você quer aprender C# ou Java?");
+}
+else {
+    alert("Você não inseriu uma área válida!");
+}
+
+const especialidadeOuFullstack = prompt("Digite 1 para seguir se especializando na área escolhida ou 2 para seguir se desenvolvendo para se tornar Fullstack");
+if (especialidadeOuFullstack == 1){
+    alert(`Continue se especializando em ${linguagem} para dominar a área de ${area}!`);
+}
+else if (especialidadeOuFullstack == 2){
+    alert(`Chegou a hora de começar a aprender outras linguagens além de ${linguagem} se você quer se tornar Fullstack!`);
+}
+else {
+    alert("Você não inseriu um valor válido!");
+}
+
+let msg = prompt("Tem mais alguma tecnologia que você gostaria de aprender? Digite 'ok' em caso positivo.");
+while (msg === "ok"){
+    let novaTecnologia = prompt("Qual?");
+    alert(`${novaTecnologia} é realmente uma tecnologia muito legal!`)
+    msg = prompt("Tem mais alguma tecnologia que você gostaria de aprender? Digite 'ok' em caso positivo.");
+}
