08ecc35d93608cabfaddfa0cfdb5a4293dc215f7
Dimas Capelari <97951537+DimasCapelari@users.noreply.github.com>
2022-04-25 16:24:56 -0300

    updates

--- Aula-de-VueJS/dom/desafio-04/app.js
+++ Aula-de-VueJS/dom/desafio-04/app.js
@@ -2,7 +2,15 @@ new Vue({
   el: '#desafio',
   data: {
     classe1: 'destaque',
-    perigo: true
+    perigo: true,
+    classe3: '',
+    classe4: '',
+    cor5: '',
+    estilo5: {
+      width: '100px',
+      height: '100px',
+    },
+    width: '0',
   },
   methods: {
     iniciarEfeito() {
@@ -11,7 +19,19 @@ new Vue({
       }, 1000)
     },
     iniciarProgresso() {
-
+      let valor = 0
+      const temporizador = setInterval(() => {
+        valor += 5
+        this.width = `${valor}%`
+        if (valor == 100) clearInterval(temporizador)
+      }, 500)
+    },
+    setPerigo(event) {
+      if (event.target.value == "true") {
+        this.perigo = true
+      } else if (event.target.value == "false") {
+        this.perigo = false
+      }
     }
   }
 })
