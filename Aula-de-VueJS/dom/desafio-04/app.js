4a7c455f0e35815d5acc9833ea35b04a361918c6
Dimas Capelari <97951537+DimasCapelari@users.noreply.github.com>
2022-04-25 15:55:23 -0300

    updates

--- Aula-de-VueJS/dom/desafio-04/app.js
+++ Aula-de-VueJS/dom/desafio-04/app.js
@@ -1,11 +1,14 @@
 new Vue({
   el: '#desafio',
   data: {
-
+    classe1: 'destaque',
+    perigo: true
   },
   methods: {
     iniciarEfeito() {
-
+      setInterval(() => {
+        this.classe1 = this.classe1 == 'destaque' ? 'encolher' : 'destaque'
+      }, 1000)
     },
     iniciarProgresso() {
 
