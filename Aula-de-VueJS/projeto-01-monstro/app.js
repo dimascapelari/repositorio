8f008b43e426d6265632f9c7dcaac50ee762453e
Dimas Capelari <97951537+DimasCapelari@users.noreply.github.com>
2022-04-28 17:10:21 -0300

    updates

--- Aula-de-VueJS/projeto-01-monstro/app.js
+++ Aula-de-VueJS/projeto-01-monstro/app.js
@@ -15,9 +15,24 @@ new Vue({
       this.running = true
       this.playerLife = 100
       this.monsterLife = 100
+    },
+    attack(especial) {
+      this.hurt('monsterLife', 5, 10, especial)
+      this.hurt('playerLife', 7, 12, false)
+    },
+    hurt(prop, min, max, especial) {
+      const plus = especial ? 5 : 0
+      const hurt = this.getRandom(min + plus, max + plus)
+      this[prop] = Math.max(this[prop] - hurt, 0)
+    },
+    getRandom(min, max) {
+      const value = Math.random() * (max - min) + min
+      return Math.round(value)
     }
   },
   watch: {
-
+    hasResult(value) {
+      if (value) this.running = false
+    }
   }
 })
