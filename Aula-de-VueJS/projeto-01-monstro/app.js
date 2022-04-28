2d0f6d8afe307261e0a624253768728c54b21452
Dimas Capelari <97951537+DimasCapelari@users.noreply.github.com>
2022-04-28 14:22:07 -0300

    updates

--- Aula-de-VueJS/projeto-01-monstro/app.js
+++ Aula-de-VueJS/projeto-01-monstro/app.js
@@ -1,10 +1,13 @@
 new Vue({
   el: '#app',
   data: {
-
+    playerLife: 10,
+    monsterLife: 0,
   },
   computed: {
-
+    hasResult() {
+      return this.playerLife == 0 || this.monsterLife == 0
+    }
   },
   methods: {
 
