1316bc253f951b576670bbedf8076ef316b8151e
Dimas Capelari <dimas.capelari@gmail.com>
2022-05-13 20:51:11 -0300

    updates

---
+++ RocketseatJavaScript/OperadoresDeComparaçãoEstritamenteIgualEstritamenteDiferente/functions.js
@@ -0,0 +1,13 @@
+// Operadores de comparação
+
+let one = 1
+let two = 2
+
+// ===  estritamente igua a
+console.log(one === "1")
+console.log(one === 1)
+
+
+// !==  estritamente diferente de
+console.log(two !== "2")
+console.log(two !== 2)
---
+++ RocketseatJavaScript/OperadoresDeComparaçãoMaiorMenor/functions.js
@@ -0,0 +1,18 @@
+// Operadores de comparação
+let one = 1
+let two = 2
+
+//  >   Maior que
+console.log(one > two)
+
+//  >=  Maior igual a
+console.log(one >= 1)
+console.log(two >= 1)
+
+//  <   Menor que
+console.log(one < two)
+
+// <=   Menor igual a
+console.log(one <= two)
+console.log(one <= 1)
+console.log(one <= 0)
