d90cfa62f2558178ef51b8ab7465cb91e3146dff
Dimas Capelari <dimas.capelari@gmail.com>
2022-05-13 17:26:42 -0300

    updates

---
+++ RocketseatJavaScript/ManipulandoArrays/functions.js
@@ -0,0 +1,27 @@
+// Manipulando Arrays
+
+let techs = ['html', 'css', 'js']
+
+// adicionar um item no fim
+techs.push('nodejs')
+
+// adicionar no começo
+techs.unshift('sql')
+
+// remover do fim
+//techs.pop()
+
+// remover do começo
+//techs.shift()
+
+// pegar somente alguns elementos do array
+// console.log(techs.slice(1, 3))
+
+// remover 1 ou mais items em qualquer posição do array
+//techs.splice(1, 2)
+
+// encontrar a posição de um elemento no array
+let index = techs.indexOf('css')
+techs.splice(index, 1)
+
+console.log(techs)
