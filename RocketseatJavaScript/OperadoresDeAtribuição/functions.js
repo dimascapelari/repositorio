0ea6e3476a8daf1fb50d468a3f05ec92c3463eca
Dimas Capelari <dimas.capelari@gmail.com>
2022-05-13 21:05:53 -0300

    updates

---
+++ RocketseatJavaScript/OperadoresDeAtribuição/functions.js
@@ -0,0 +1,26 @@
+// Operadores de atribuição (Assignment)
+let x
+
+// assignment
+x = 1
+
+// addition assignment
+// x = x + 2
+x += 2
+
+// subtraction assignment
+// x = x - 1
+x -= 1
+
+// multiplication assignment
+// x = x * 2
+x *= 2
+
+// division assignment
+// x = x / 2
+x /= 2
+
+// remainder, exponetiation
+//x %= 2
+x **= 2
+console.log(x)
