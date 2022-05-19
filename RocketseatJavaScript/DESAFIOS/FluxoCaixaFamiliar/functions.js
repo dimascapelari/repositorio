8f2caac1bb4f88793c8ee50ba59d1d4324308be3
Dimas Capelari <dimas.capelari@gmail.com>
2022-05-18 22:04:39 -0300

    up

--- RocketseatJavaScript/DESAFIOS/FluxoCaixaFamiliar/functions.js
+++ RocketseatJavaScript/DESAFIOS/FluxoCaixaFamiliar/functions.js
@@ -9,4 +9,36 @@ Agora, crie uma função que irá calcular o total de receitas
 e despesas e irá mostrar uma mensagem se a família está com
 saldo positivo ou negativo, seguindo o valor do saldo*/
 
+let family = {
+    renda: [2500, 3200, 250.43, 360.45],
+    despesas: [320.34, 128.45, 176.87, 1450.00, 500]
+}
 
+function soma(array) {
+    let total = 0
+
+    for (let value of array) {
+        total += value
+    }
+
+    return total
+}
+
+function calculateBalance() {
+    const calculaRenda = soma(family.renda)
+    const calculaDespesas = soma(family.despesas)
+
+    const total = calculaRenda - calculaDespesas
+
+    const itsOK = total >= 0
+
+    let balanceText = 'negativo'
+
+    if (itsOK) {
+        balanceText = 'positivo'
+    }
+
+    console.log(`Seu ${balanceText}: R$ ${total.toFixed(2)}`)
+}
+
+calculateBalance()
