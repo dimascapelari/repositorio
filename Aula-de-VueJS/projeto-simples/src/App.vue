5ad1ef4a6e03f546f66374b384de6fd9f2ddb90f
Dimas Capelari <dimas.capelari@gmail.com>
2022-05-20 22:05:59 -0300

    updates

--- Aula-de-VueJS/projeto-simples/src/App.vue
+++ Aula-de-VueJS/projeto-simples/src/App.vue
@@ -1,28 +1,23 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    {{ titulo }}
+    <button @click="titulo += '#'">Alterar</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data: function () {
+    return {
+      titulo: "Teste Data usando Vue.JS!!!",
+    };
+  },
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  background-color: chocolate;
+  color: #fff;
 }
 </style>
