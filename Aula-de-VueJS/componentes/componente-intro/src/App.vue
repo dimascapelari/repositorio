4d9f114d2bd45b99fcd89092aca81c217d24dc5d
Dimas Capelari <dimas.capelari@gmail.com>
2022-05-21 20:48:56 -0300

    up

---
+++ Aula-de-VueJS/componentes/componente-intro/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <h1>Contadores</h1>
+    {{ contador }}
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      contador: 0,
+    };
+  },
+};
+</script>
+
+<style>
+</style>
