d5a9be9ceba91d38d370abc6498c9fb282168976
Dimas Capelari <dimas.capelari@gmail.com>
2022-05-23 14:46:08 -0300

    citacoes

--- Aula-de-VueJS/componentes/componentes-avancados/citacoes/src/components/Citacao.vue
+++ Aula-de-VueJS/componentes/componentes-avancados/citacoes/src/components/Citacao.vue
@@ -1,20 +1,18 @@
 <template>
-    <div class="citacao">
-        
-    </div>
+  <div class="citacao">
+    <slot></slot>
+  </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style scoped>
-    .citacao {
-        border: 1px solid #DDD;
-        background-color: rgba(0, 0, 0, .1);
-        padding: 25px;
-        max-width: 450px;
-    }
+.citacao {
+  border: 1px solid #ddd;
+  background-color: rgba(0, 0, 0, 0.1);
+  padding: 25px;
+  max-width: 450px;
+}
 </style>
--- Aula-de-VueJS/componentes/componentes-avancados/citacoes/src/components/Citacoes.vue
+++ Aula-de-VueJS/componentes/componentes-avancados/citacoes/src/components/Citacoes.vue
@@ -1,45 +1,57 @@
 <template>
-    <div class="citacoes">
-        <span>
-            <button @click="numero--">&lt;</button>
-            <button @click="numero++">&gt;</button>
-        </span>
-        {{ numero }} {{ indice }}
-    </div>
+  <div class="citacoes">
+    <span>
+      <button @click="numero--">&lt;</button>
+      <button @click="numero++">&gt;</button>
+    </span>
+    <Citacao>
+      <h1>{{ citacoes[indice].autor }}</h1>
+      <p>{{ citacoes[indice].texto }}</p>
+      <h6>{{ citacoes[indice].fonte }}</h6>
+    </Citacao>
+  </div>
 </template>
 
 <script>
+import Citacao from "./Citacao.vue";
+
 export default {
-    data() {
-        return {
-            numero: 0,
-            citacoes: [{
-                fonte: 'Jornal do Empreendedor',
-                texto: 'Lembre-se sempre que você é absolutamente único. Assim como todos os outros.',
-                autor: 'Margaret Mead'
-            }, { 
-                fonte: 'Frases de Mãe',
-                texto: 'Isso não é um quarto, é um chiqueiro.',
-                autor: 'Roberta'
-            }, {
-                fonte: 'Frases de Pai',
-                texto: 'Vou contar até 3! 1, 2, 2...',
-                autor: 'Gustavo'
-            }]
-        }
+  components: { Citacao },
+  data() {
+    return {
+      numero: 0,
+      citacoes: [
+        {
+          fonte: "Jornal do Empreendedor",
+          texto:
+            "Lembre-se sempre que você é absolutamente único. Assim como todos os outros.",
+          autor: "Margaret Mead",
+        },
+        {
+          fonte: "Frases de Mãe",
+          texto: "Isso não é um quarto, é um chiqueiro.",
+          autor: "Roberta",
+        },
+        {
+          fonte: "Frases de Pai",
+          texto: "Vou contar até 3! 1, 2, 2...",
+          autor: "Gustavo",
+        },
+      ],
+    };
+  },
+  computed: {
+    indice() {
+      return Math.abs(this.numero % 3);
     },
-    computed: {
-        indice() {
-            return Math.abs(this.numero % 3)
-        }
-    }
-}
+  },
+};
 </script>
 
 <style scoped>
-    .citacoes {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-    }
+.citacoes {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
 </style>
