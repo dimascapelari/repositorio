0803faa1d7f661415a2844ac21b896014faaa1c6
Dimas Capelari <dimas.capelari@gmail.com>
2022-05-22 21:27:17 -0300

    updates

---
+++ Aula-de-VueJS/componentes/componente-desafio/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-app>
+    <app-menu />
+    <app-toolbar />
+    <v-content>
+      <app-carousel />
+    </v-content>
+    <app-footer />
+  </v-app>
+</template>
+
+<script>
+import Carousel from "@/components/widgets/CompCarrousel.vue";
+import Menu from "@/components/template/CompMenu.vue";
+import Toolbar from "@/components/template/CompToolbar.vue";
+import Footer from "@/components/template/CompoFooter.vue";
+
+export default {
+  components: {
+    "app-footer": Footer,
+    "app-carousel": Carousel,
+    "app-toolbar": Toolbar,
+    "app-menu": Menu,
+  },
+};
+</script>
---
+++ Aula-de-VueJS/componentes/componente-desafio/src/components/template/CompoFooter.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-footer class="pa-3" app>
+    <v-spacer></v-spacer>
+    <div>Curso Vue &copy; {{ new Date().getFullYear() }}</div>
+  </v-footer>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
