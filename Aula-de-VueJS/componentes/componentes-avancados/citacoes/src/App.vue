6727742aa2a119c4ef917bc15d6908d2201a81fc
Dimas Capelari <dimas.capelari@gmail.com>
2022-05-23 16:14:12 -0300

    up

--- Aula-de-VueJS/componentes/componentes-avancados/citacoes/src/App.vue
+++ Aula-de-VueJS/componentes/componentes-avancados/citacoes/src/App.vue
@@ -1,37 +1,47 @@
 <template>
-	<div id="app">
-		<Citacoes />
-		<Sobre />
-	</div>
+  <div id="app">
+    <span>
+      <button @click="componente = 'Citacoes'">Citações</button>
+      <button @click="componente = 'Sobre'">Sobre</button>
+    </span>
+    <keep-alive>
+      <component :is="componente" />
+    </keep-alive>
+  </div>
 </template>
 
 <script>
-import Citacoes from './components/Citacoes'
-import Sobre from './components/Sobre'
+import Citacoes from "./components/Citacoes";
+import Sobre from "./components/Sobre";
 
 export default {
-	components: { Citacoes, Sobre }
-}
+  components: { Citacoes, Sobre },
+  data() {
+    return {
+      componente: "Citacoes",
+    };
+  },
+};
 </script>
 
 <style>
-	#app {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		height: 100vh;
-		font-size: 1.6rem;
-	}
+#app {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  font-size: 1.6rem;
+}
 
-	button {
-        outline: none;
-        padding: 5px 15px 10px;
-        margin: 10px 5px;
-        border-radius: 3px;
-        font-size: 2rem;
-        font-weight: 300;
-        color: #FFF;
-        background-color: rgba(0, 0, 0, .5);
-    }
+button {
+  outline: none;
+  padding: 5px 15px 10px;
+  margin: 10px 5px;
+  border-radius: 3px;
+  font-size: 2rem;
+  font-weight: 300;
+  color: #fff;
+  background-color: rgba(0, 0, 0, 0.5);
+}
 </style>
--- Aula-de-VueJS/componentes/componentes-avancados/citacoes/src/components/Citacao.vue
+++ Aula-de-VueJS/componentes/componentes-avancados/citacoes/src/components/Citacao.vue
@@ -2,12 +2,25 @@
   <div class="citacao">
     <slot name="autor"></slot>
     <slot name="texto"></slot>
-    <slot name="fonte"></slot>
+    <div class="fonte"><slot name="fonte"></slot></div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  created() {
+    console.log("created");
+  },
+  destroyed() {
+    console.log("destroyed");
+  },
+  activated() {
+    console.log("activated");
+  },
+  deactivated() {
+    console.log("deactivated");
+  },
+};
 </script>
 
 <style scoped>
@@ -16,5 +29,9 @@ export default {};
   background-color: rgba(0, 0, 0, 0.1);
   padding: 25px;
   max-width: 450px;
+}
+
+.fonte {
+  float: right;
 }
 </style>
