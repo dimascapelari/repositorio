a5719bca8150600f668ae568a01bb954b5a02a7f
Dimas Capelari <dimas.capelari@gmail.com>
2022-05-22 21:10:40 -0300

    updates

--- Aula-de-VueJS/componentes/componente-desafio/src/App.vue
+++ Aula-de-VueJS/componentes/componente-desafio/src/App.vue
@@ -1,68 +1,26 @@
 <template>
   <v-app>
-    <v-navigation-drawer app>
-      <v-toolbar flat>
-        <v-list>
-          <v-list-tile>
-            <v-list-tile-title class="title">
-                Menu
-            </v-list-tile-title>
-          </v-list-tile>
-        </v-list>
-      </v-toolbar>
-
-      <v-divider></v-divider>
-
-      <v-list dense class="pt-0">
-        <v-list-item v-for="item in itensMenu" :key="item.titulo">
-          <v-list-item-icon>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-icon>
-          <v-list-item-content>
-            <v-list-item-title>{{ item.titulo }}</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-
-    <v-app-bar app>
-        <v-toolbar-title class="headline text-uppercase">
-				<span>Desafio</span>
-				<span class="font-weight-light">Componente</span>
-        </v-toolbar-title>
-    </v-app-bar>
-
+    <app-menu />
+    <app-toolbar />
     <v-content>
-      <v-carousel>
-        <v-carousel-item
-            v-for="(item,i) in fotos"
-            :key="i"
-            :src="item.src"></v-carousel-item>
-      </v-carousel>
+      <app-carousel />
     </v-content>
-
-    <v-footer class="pa-3" app>
-      <v-spacer></v-spacer>
-      <div>Curso Vue &copy; {{ new Date().getFullYear() }}</div>
-    </v-footer>
+    <app-footer />
   </v-app>
 </template>
 
 <script>
+import Carousel from "./CompCarrousel.vue";
+import Menu from "./CompMenu.vue";
+import Toolbar from "./CompToolbar.vue";
+import Footer from "./CompoFooter.vue";
+
 export default {
-  data() {
-    return {
-      itensMenu: [
-        {titulo: 'Início', icon: 'mdi-view-dashboard'},
-        {titulo: 'Sobre', icon: 'mdi-forum'}
-      ],
-      fotos: [
-        {src: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg'},
-        {src: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg'},
-        {src: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg'},
-        {src: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg'}
-      ]
-    }
-  }
-}
+  components: {
+    "app-footer": Footer,
+    "app-carousel": Carousel,
+    "app-toolbar": Toolbar,
+    "app-menu": Menu,
+  },
+};
 </script>
