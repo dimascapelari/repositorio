bb8d512af85846a3af10276ddff0a45ad5f399fb
Dimas Capelari <dimas.capelari@gmail.com>
2022-05-23 20:14:00 -0300

    updates

--- Aula-de-VueJS/projeto-02-tarefas/tarefas/src/components/TaskGrid.vue
+++ Aula-de-VueJS/projeto-02-tarefas/tarefas/src/components/TaskGrid.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="task-grid">
-    <template>
+    <template v-if="tasks.length">
       <Task v-for="task in tasks" :key="task.name" :task="task"></Task>
     </template>
+    <p v-else class="no-task">Sua vida está em dia :)</p>
   </div>
 </template>
 
@@ -18,4 +19,18 @@ export default {
 </script>
 
 <style>
+.task-grid {
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+}
+
+.task-grid .task {
+  margin: 10px;
+}
+
+.no-task {
+  color: #aaa;
+  font-size: 1.7rem;
+}
 </style>
