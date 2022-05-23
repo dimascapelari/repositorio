1a5edc8e69f4fd8cb41d48a8b1d55bc8ec570118
Dimas Capelari <dimas.capelari@gmail.com>
2022-05-23 18:37:43 -0300

    updates

--- Aula-de-VueJS/projeto-02-tarefas/tarefas/src/App.vue
+++ Aula-de-VueJS/projeto-02-tarefas/tarefas/src/App.vue
@@ -1,34 +1,45 @@
 <template>
-	<div id="app">
-		<h1>Tarefas</h1>
-	</div>
+  <div id="app">
+    <h1>Tarefas</h1>
+    <TaskGrid :tasks="tasks" />
+  </div>
 </template>
 
 <script>
-export default {
+import TaskGrid from "./components/TaskGrid.vue";
 
-}
+export default {
+  components: { TaskGrid },
+  data() {
+    return {
+      tasks: [
+        { name: "Lavar a louça", pending: false },
+        { name: "Comprar blusa", pending: true },
+      ],
+    };
+  },
+};
 </script>
 
 <style>
-	body {
-		font-family: 'Lato', sans-serif;
-		background: linear-gradient(to right, rgb(22, 34, 42), rgb(58, 96, 115));
-		color: #FFF;
-	}
+body {
+  font-family: "Lato", sans-serif;
+  background: linear-gradient(to right, rgb(22, 34, 42), rgb(58, 96, 115));
+  color: #fff;
+}
 
-	#app {
-		display: flex;
-		flex: 1;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		height: 100vh;
-	}
+#app {
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
 
-	#app h1 {
-		margin-bottom: 5px;
-		font-weight: 300;
-		font-size: 3rem;
-	}
+#app h1 {
+  margin-bottom: 5px;
+  font-weight: 300;
+  font-size: 3rem;
+}
 </style>
