fd47b8a8c2f4a315e2a01e2b435fb07097036d87
Dimas Capelari <dimas.capelari@gmail.com>
2022-06-07 16:19:19 -0300

    exe-01

---
+++ VueJS-Portugal/vuecli001/exe-01/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <button @click="clickNome()">Nome</button>
+    <button @click="clickApedido()">Apelido</button>
+    <infoPessoa v-bind:infonome="nome" v-bind:infoapelido="apelido" />
+  </div>
+</template>
+
+<script>
+import infoPessoa from "./components/InfoPessoa.vue";
+
+export default {
+  components: {
+    infoPessoa,
+  },
+  data: function () {
+    return {
+      nome: "",
+      apelido: "",
+    };
+  },
+  methods: {
+    clickNome() {
+      this.nome = "Dimas";
+    },
+    clickApedido() {
+      this.apelido = "Capelari";
+    },
+  },
+};
+</script>
---
+++ VueJS-Portugal/vuecli001/exe-01/src/components/InfoPessoa.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <p>{{ infonome }} {{ infoapelido }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["infonome", "infoapelido"],
+};
+</script>
