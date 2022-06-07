23ddb1f0ae99379e9b177a85ab1cb07fef7d3c49
Dimas Capelari <dimas.capelari@gmail.com>
2022-06-07 20:41:26 -0300

    exe-03

---
+++ VueJS-Portugal/vuecli001/exe-03/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <p>componente principal</p>
+
+    <CompoNente :valor1="nome" />
+  </div>
+</template>
+
+<script>
+import CompoNente from "./components/CompoNente.vue";
+
+export default {
+  data: function () {
+    return {
+      nome: "Dimas",
+    };
+  },
+  components: {
+    CompoNente,
+  },
+};
+</script>
---
+++ VueJS-Portugal/vuecli001/exe-03/src/components/CompoNente.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <p>componente child</p>
+    <p>{{ valor1 }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    valor1: {
+      type: String,
+      default: "Dimas Capelari",
+    },
+  },
+};
+</script>
