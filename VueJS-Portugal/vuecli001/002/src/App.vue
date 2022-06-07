b300c8d56def78cc4c24f0b3dd79b1f34ce11b3e
Dimas Capelari <dimas.capelari@gmail.com>
2022-06-07 10:33:28 -0300

    002

---
+++ VueJS-Portugal/vuecli001/002/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h3>Título</h3>
+    <div class="alert alert-success text-center p-3">Mensagem</div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
