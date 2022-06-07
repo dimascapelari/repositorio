13743ead867793005412ac83b9b4858eedb58b6f
Dimas Capelari <dimas.capelari@gmail.com>
2022-06-07 15:32:14 -0300

    PARENT PARA CHILD COMPONENTS

---
+++ VueJS-Portugal/vuecli001/006/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="container mt-3 box">
+    <div class="row">
+      <div class="col-12 p-4">
+        <p>Mecanismo para alterar o valor do nome.</p>
+        <button @click="alterarNome">Alterar nome</button>
+        <CompDetalhe v-bind:meunome="onomeoriginal" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import CompDetalhe from "./components/CompDetalhe.vue";
+
+export default {
+  components: {
+    CompDetalhe,
+  },
+  data: function () {
+    return {
+      onomeoriginal: "Dimas",
+    };
+  },
+  methods: {
+    alterarNome() {
+      this.onomeoriginal = "Capelari";
+    },
+  },
+};
+</script>
+
+<style scoped>
+.box {
+  border: 2px solid black;
+  background-color: rgb(200, 200, 200);
+}
+</style>
---
+++ VueJS-Portugal/vuecli001/006/src/components/CompDetalhe.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="row box1 mt-3 p-3">
+    <p>O meu nome é: {{ meunome }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["meunome"],
+};
+</script>
+
+<style scoped>
+.box1 {
+  border: 1px solid black;
+  background-color: rgb(180, 180, 180);
+}
+</style>
