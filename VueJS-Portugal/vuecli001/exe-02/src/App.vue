0af5e0094a4b0b834f8359c48596c0aed8aba2c4
Dimas Capelari <dimas.capelari@gmail.com>
2022-06-07 20:18:07 -0300

    exe-02

---
+++ VueJS-Portugal/vuecli001/exe-02/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <button @click="executar(0)" class="btn btn-primary m-1">Click 0</button>
+    <button @click="executar(100)" class="btn btn-primary m-1">
+      Click 100
+    </button>
+    <button @click="executar(200)" class="btn btn-primary m-1">
+      Click 200
+    </button>
+
+    <CompoNente :meuvalor="valor" />
+  </div>
+</template>
+
+<script>
+import CompoNente from "./components/CompoNente.vue";
+
+export default {
+  components: {
+    CompoNente,
+  },
+  data: function () {
+    return {
+      valor: 0,
+    };
+  },
+  methods: {
+    executar(recebeValor) {
+      this.valor = recebeValor;
+      console.log(this.valor);
+    },
+  },
+};
+</script>
---
+++ VueJS-Portugal/vuecli001/exe-02/src/components/CompoNente.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="alert alert-info">
+    <p>{{ analisar() }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["meuvalor"],
+  methods: {
+    analisar() {
+      if (this.meuvalor < 100) {
+        return "Menor que 100!";
+      } else if (this.meuvalor == 100) {
+        return "Igual a 100!";
+      } else {
+        return "Maior que 100!";
+      }
+    },
+  },
+};
+</script>
