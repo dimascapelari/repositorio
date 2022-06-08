54d5017ec74d1116045a85c662e76534bf1e2d0b
Dimas Capelari <dimas.capelari@gmail.com>
2022-06-07 21:42:55 -0300

    exe-04 solução Dimas

---
+++ VueJS-Portugal/vuecli001/exe-04/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <p>(App.vue) Nome = {{ nome }}</p>
+    <button @click="alterar()">Alterar</button>
+    <hr />
+    <CompoNente :meunome="nome" @nomeAlterado="nome = $event" />
+  </div>
+</template>
+
+<script>
+import CompoNente from "./components/CompoNente.vue";
+
+export default {
+  data: function () {
+    return {
+      nome: "Dimas",
+    };
+  },
+  components: {
+    CompoNente,
+  },
+  methods: {
+    alterar() {
+      this.nome = "Capelari";
+    },
+  },
+};
+</script>
---
+++ VueJS-Portugal/vuecli001/exe-04/src/components/CompoNente.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <p>(CompoNente.vue) Nome: {{ meunome }}</p>
+    <button @click="alterar2()">Alterar 2</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["meunome"],
+
+  //----------------------------------------------------------------------------------
+  // Criei um data para pegar a PROPS e mudar o ['meunome'] depois segui com o código
+  // a partir dessa mudança para eliminar o erro que estava dando dizendo
+  // que não podemos mudar uma props diretamente... (Solução Dimas 07/06/2022).
+  data: function () {
+    return {
+      nomeprops: this.meunome,
+    };
+  },
+  //----------------------------------------------------------------------------------
+  methods: {
+    alterar2() {
+      this.nomeprops = "Batera";
+      this.$emit("nomeAlterado", this.nomeprops);
+    },
+  },
+};
+</script>
