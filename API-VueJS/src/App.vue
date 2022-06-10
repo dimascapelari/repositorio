5477caa06e467b9d72037208fce77c5c80577914
Dimas Capelari <dimas.capelari@gmail.com>
2022-06-10 13:21:38 -0300

    API-VueJS

---
+++ API-VueJS/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <ul v-for="produto in produtos" :key="produto.id">
+      <li>{{ produto.nome }}</li>
+      <li>{{ produto.valor }}</li>
+      <li>{{ produto.tamanho }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      produtos: [],
+    };
+  },
+  async mounted() {
+    const resp = await fetch("http://localhost:3000/produtos");
+    const response = await resp.json();
+    this.produtos = response;
+  },
+};
+</script>
+
+<style>
+</style>
