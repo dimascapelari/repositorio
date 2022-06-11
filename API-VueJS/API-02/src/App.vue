098511c9e1f7d7b100e14f2fc35a8e970dc9ecd9
Dimas Capelari <dimas.capelari@gmail.com>
2022-06-11 09:06:12 -0300

    api-02

---
+++ API-VueJS/API-02/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <ul v-for="profissional in profissionais" :key="profissional.id">
+      <li>ID: {{ profissional.id }}</li>
+      <li>Nome: {{ profissional.nome }}</li>
+      <li>CPF: {{ profissional.cpf }}</li>
+      <li>Cidade: {{ profissional.cidadeId }}</li>
+      <li>Especialidade: {{ profissional.especialidadeId }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      profissionais: [],
+    };
+  },
+  async mounted() {
+    const resp = await fetch(
+      "https://api-teste-front-end-fc.herokuapp.com/profissionais"
+    );
+    const response = await resp.json();
+    this.profissionais = response;
+  },
+};
+</script>
+
+<style>
+</style>
