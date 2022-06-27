dc2bd52cfe9af8a6df64cba3f7dc73b3acc5391a
Dimas Capelari <dimas.capelari@gmail.com>
2022-06-27 11:22:09 -0300

    event.preventDefault() no index.js raiz

--- newDev/modulo-03/index.js
+++ newDev/modulo-03/index.js
@@ -3,7 +3,10 @@ import { CadastroComponent } from "./controller/cadastro.js";
 const link = document.getElementById('link')
 
 link.addEventListener('click', (event) => {
+    event.preventDefault()
+
     const option = event.path[0].innerText
+    console.log(event.path[0].innerText)
 
     switch (option) {
         case "Cadastro":
