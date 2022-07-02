e3db4cec691e8f4981edd19d72190ee907f56da1
Dimas Capelari <dimas.capelari@gmail.com>
2022-07-02 17:01:02 -0300

    up

---
+++ ProgramandoSolucoes/JavaScript/SwitchCase/script.js
@@ -0,0 +1,44 @@
+
+// ----- Sintaxe padrão -----
+// let numero = 2
+
+// switch (numero) {
+//     case 1:
+//         console.log('número 1')
+//         break
+//     case 2:
+//         console.log('número 2')
+//         break
+// }
+
+
+
+// ----- Switch case com o valor default -----
+// let numero = 3
+
+// switch (numero) {
+//     case 1:
+//         console.log('número 1')
+//         break
+//     case 2:
+//         console.log('número 2')
+//         break
+//     default:
+//         console.log('o número não é nem 1 e nem 2')
+//         break
+// }
+
+
+
+let numero = 1
+
+switch (numero) {
+    case 1:
+    case 2:
+    case 3:
+        console.log('o número é 1, 2 ou 3')
+        break
+    default:
+        console.log('o número não é nem 1, nem 2 e nem 3')
+        break
+}
