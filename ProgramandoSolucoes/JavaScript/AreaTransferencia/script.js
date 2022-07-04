bd5d8116a7e6577b3da70c64e57a0aef38eadd92
Dimas Capelari <dimas.capelari@gmail.com>
2022-07-04 06:56:00 -0300

    AreaTransferencia

---
+++ ProgramandoSolucoes/JavaScript/AreaTransferencia/script.js
@@ -0,0 +1,8 @@
+function copiarTexto() {
+    let textoCopiado = document.getElementById('texto')
+    textoCopiado.select()
+    textoCopiado.setSelectionRange(0, 99999)
+
+    document.execCommand("copy")
+    alert(`O texto copiado é ${textoCopiado.value}`)
+}
