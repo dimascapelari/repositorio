1a572a8a7f46782834325953d7aba79a6c99582f
Dimas Capelari <dimas.capelari@gmail.com>
2022-07-07 11:34:58 -0300

    Create retornar-o-padro-inicial-para-functions.js

---
+++ freeCodeCamp/retornar-o-padro-inicial-para-functions.js
@@ -0,0 +1,14 @@
+// Configuração
+function abTest(a, b) {
+    // Altere apenas o código abaixo desta linha
+
+    if (a < 0 || b < 0) {
+        return undefined;
+    }
+
+    // Altere apenas o código acima desta linha
+
+    return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
+}
+
+abTest(2, 2);
