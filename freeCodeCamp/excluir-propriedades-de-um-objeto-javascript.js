5f1890aee321f0e3839d30c83f5325a655f9bf0b
Dimas Capelari <dimas.capelari@gmail.com>
2022-07-07 23:19:18 -0300

    Create excluir-propriedades-de-um-objeto-javascript.js

---
+++ freeCodeCamp/excluir-propriedades-de-um-objeto-javascript.js
@@ -0,0 +1,13 @@
+// Configuração
+const myDog = {
+  "name": "Happy Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"],
+  "bark": "woof"
+};
+
+// Altere apenas o código abaixo desta linha
+
+
+delete myDog.tails
