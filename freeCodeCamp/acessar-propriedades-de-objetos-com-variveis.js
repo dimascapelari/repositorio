f9aaa97642175b4cb95e42ac830019dc346cb447
Dimas Capelari <dimas.capelari@gmail.com>
2022-07-08 09:09:07 -0300

    Create acessar-propriedades-de-objetos-com-variveis.js

---
+++ freeCodeCamp/acessar-propriedades-de-objetos-com-variveis.js
@@ -0,0 +1,10 @@
+// Configuração
+const testObj = {
+  12: "Namath",
+  16: "Montana",
+  19: "Unitas"
+};
+
+// Altere apenas o código abaixo desta linha
+const playerNumber = 16;  // Altere esta linha
+const player = testObj[playerNumber];   // Altere esta linha
