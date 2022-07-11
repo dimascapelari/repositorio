6798902430d6544f688648a960eeb1fd691c27b4
Dimas Capelari <dimas.capelari@gmail.com>
2022-07-11 08:21:38 -0300

    Create gerar-frases-aleatorias-com-javascript.js

---
+++ freeCodeCamp/gerar-frases-aleatorias-com-javascript.js
@@ -0,0 +1,11 @@
+function randomFraction() {
+
+  // Altere apenas o código abaixo desta linha
+ 
+
+  return Math.random();
+
+  // Altere apenas o código acima desta linha
+}
+
+console.log(randomFraction())
