d64f41cb9b1c92e3409ed9d8bbbd7be0896d5df4
Dimas Capelari <dimas.capelari@gmail.com>
2022-07-12 08:46:17 -0300

    desafioNumerosDaSorte

---
+++ desafioNumerosDaSorte/script.js
@@ -0,0 +1,13 @@
+let btn = document.getElementById('btn')
+let resultado = document.getElementById('resultado')
+
+
+function sorte() {
+    resultado.innerHTML = `Resultado: Olá Mundo`
+}
+
+
+btn.addEventListener('click', sorte)
+
+
+// https://apilotofacil.herokuapp.com/?q_combinacoes=10&q_dezenas=15
