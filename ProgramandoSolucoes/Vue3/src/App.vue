7634eef2d161fdb4507d8c3f725afc6cb0f5093d
Dimas Capelari <dimas.capelari@gmail.com>
2022-07-12 15:15:17 -0300

    Vue3-Suspense

---
+++ ProgramandoSolucoes/Vue3/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+
+  <Suspense>
+    <template #default>
+      <ListProductsComponent />
+    </template>
+    <template #fallback>
+      <div>
+        <h1>Loading ...</h1>
+      </div>
+    </template>
+  </Suspense>
+</template>  
+
+<script>
+import ListProductsComponent from "./components/ListProductsComponent.vue";
+
+export default {
+  name: "App",
+  components: {
+    ListProductsComponent,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ ProgramandoSolucoes/Vue3/src/components/ListProductsComponent.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>Teste de componente</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ListProductsComponent",
+  async setup() {
+    const promise = new Promise((resolve) => {
+      setTimeout(function () {
+        resolve("Success");
+      }, 3000);
+    });
+
+    const product = await promise;
+
+    return {
+      product,
+    };
+  },
+};
+</script>
+
+<style>
+</style>
