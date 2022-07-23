f8f89bfc681fd51e8259e7d91d6f98148b938ed2
Dimas Capelari <dimas.capelari@gmail.com>
2022-07-23 10:20:35 -0300

    Create iterar-atravs-de-um-array-com-lao-for.js

---
+++ freeCodeCamp/iterar-atravs-de-um-array-com-lao-for.js
@@ -0,0 +1,10 @@
+// Configuração
+const myArr = [2, 3, 4, 5, 6];
+
+// Altere apenas o código abaixo desta linha
+let total = 0
+
+for (let i = 0; i < myArr.length; i++ ){
+  console.log(total + myArr[i])
+  total = total + myArr[i]
+}
