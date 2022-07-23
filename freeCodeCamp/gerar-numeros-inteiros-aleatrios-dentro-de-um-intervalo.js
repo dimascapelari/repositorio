2dd8ec4bf056df07677acc5dd1bf11ebfb4dfcfc
Dimas Capelari <dimas.capelari@gmail.com>
2022-07-23 10:15:10 -0300

    Create gerar-numeros-inteiros-aleatrios-dentro-de-um-intervalo.js

---
+++ freeCodeCamp/gerar-numeros-inteiros-aleatrios-dentro-de-um-intervalo.js
@@ -0,0 +1,15 @@
+function randomRange(myMin, myMax) {
+  // Altere apenas o código abaixo desta linha
+
+  return Math.floor(Math.random() * (myMax - myMin + 1)) + myMin;
+
+
+  // Altere apenas o código acima desta linha
+}
+
+  let contador = randomRange(1, 15);
+
+
+ 
+
+console.log(contador);
