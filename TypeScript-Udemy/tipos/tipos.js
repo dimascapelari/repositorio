28db101484c40c73b41ad95721007f6e54538947
Dimas Capelari <dimas.capelari@gmail.com>
2022-07-27 20:10:00 -0300

    Desafio

--- TypeScript-Udemy/tipos/tipos.js
+++ TypeScript-Udemy/tipos/tipos.js
@@ -159,3 +159,16 @@ podeSerNulo = 12;
 console.log(podeSerNulo);
 podeSerNulo = 'abc';
 console.log(podeSerNulo);
+let contaBancaria = {
+    saldo: 3456,
+    depositar(valor) {
+        this.saldo += valor;
+    }
+};
+let correntista = {
+    nome: 'Ana Silva',
+    contaBancaria: contaBancaria,
+    contatos: ['34567890', '98765432']
+};
+correntista.contaBancaria.depositar(3000);
+console.log(correntista);
--- TypeScript-Udemy/tipos/tipos.ts
+++ TypeScript-Udemy/tipos/tipos.ts
@@ -256,3 +256,32 @@ podeSerNulo = 12
 console.log(podeSerNulo)
 podeSerNulo = 'abc'
 console.log(podeSerNulo)
+
+
+// Desafio
+type ContaBancaria = {
+    saldo: number,
+    depositar: (valor: number) => void
+}
+
+let contaBancaria: ContaBancaria = {
+    saldo: 3456,
+    depositar(valor: number) {
+        this.saldo += valor
+    }
+}
+
+type Correntista = {
+    nome: string,
+    contaBancaria: ContaBancaria,
+    contatos: string[]
+}
+
+let correntista: Correntista = {
+    nome: 'Ana Silva',
+    contaBancaria: contaBancaria,
+    contatos: ['34567890', '98765432']
+}
+
+correntista.contaBancaria.depositar(3000)
+console.log(correntista)
