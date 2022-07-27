75abc9f92a59c0f2f4d6fe41084619fdda727cb3
Dimas Capelari <dimas.capelari@gmail.com>
2022-07-27 16:49:56 -0300

    desafio

--- TypeScript-Udemy/tipos/tipos.js
+++ TypeScript-Udemy/tipos/tipos.js
@@ -87,3 +87,44 @@ usuario = {
     nome: 'Maria'
 };
 console.log(usuario);
+// Desafio
+/*
+    Criar um objeto funcionário com:
+    - Array de strings com os nomes dos supervisores
+    - Função de bater ponto que recebe a hora (número)
+      e retorna uma string
+       -> Ponto normal (<= 8)
+       -> Fora do horário (> 8)
+*/
+// ----- Minha resposta -----
+/*
+let funcionario: { nome1: string, nome2: string, nome3: string } = {
+    nome1: 'João',
+    nome2: 'José',
+    nome3: 'Maria'
+}
+
+function baterPonto(hora: number) {
+    if (hora <= 8) {
+        return "Ponto normal"
+    } else if (hora > 8) {
+        return "Fora do horário"
+    }
+}
+console.log(baterPonto(9))
+*/
+// ----- ----- ----- -----
+let funcionario = {
+    supervisores: ['Ana', 'Fernando'],
+    baterPonto(horario) {
+        if (horario <= 8) {
+            return 'Ponto normal';
+        }
+        else {
+            return 'Fora do horário!';
+        }
+    }
+};
+console.log(funcionario.supervisores);
+console.log(funcionario.baterPonto(8));
+console.log(funcionario.baterPonto(9));
