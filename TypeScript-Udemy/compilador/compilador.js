52031e2e605aa902de6e792bab1365c837889ab7
Dimas Capelari <dimas.capelari@gmail.com>
2022-07-28 08:51:20 -0300

    compilador

---
+++ TypeScript-Udemy/compilador/compilador.js
@@ -0,0 +1,6 @@
+"use strict";
+let canal = 'Gaveta';
+let inscritos = 610234;
+// canal = inscritos
+console.log(`Canal = ${canal}`);
+// let nome = 'Pedro'
---
+++ TypeScript-Udemy/compilador/compilador.ts
@@ -0,0 +1,7 @@
+let canal: string = 'Gaveta'
+let inscritos: number = 610234
+
+// canal = inscritos
+console.log(`Canal = ${canal}`)
+
+// let nome = 'Pedro'
