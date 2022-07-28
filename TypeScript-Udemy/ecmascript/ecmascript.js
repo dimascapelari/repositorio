6606a2c1128fcddbb73490b32f767b66faab49e1
Dimas Capelari <dimas.capelari@gmail.com>
2022-07-28 14:26:00 -0300

    Destructuring (array)

--- TypeScript-Udemy/ecmascript/ecmascript.js
+++ TypeScript-Udemy/ecmascript/ecmascript.js
@@ -105,4 +105,11 @@ function tuplaParam2(...params) {
     console.log(`2) ${params[0]} ${params[1]} ${params[2]}`);
 }
 tuplaParam2(...tupla);
+// Destructuring (array)
+const caracteristicas = ['Motor Zetec 1.8', 2020];
+// const motor = caracteristicas[0]
+// const ano = caracteristicas[1]
+const [motor, ano] = caracteristicas;
+console.log(motor);
+console.log(ano);
 //# sourceMappingURL=ecmascript.js.map
--- TypeScript-Udemy/ecmascript/ecmascript.ts
+++ TypeScript-Udemy/ecmascript/ecmascript.ts
@@ -138,3 +138,13 @@ function tuplaParam2(...params: [number, string, boolean]) {
     console.log(`2) ${params[0]} ${params[1]} ${params[2]}`)
 }
 tuplaParam2(...tupla)
+
+
+// Destructuring (array)
+const caracteristicas = ['Motor Zetec 1.8', 2020]
+// const motor = caracteristicas[0]
+// const ano = caracteristicas[1]
+
+const [motor, ano] = caracteristicas
+console.log(motor)
+console.log(ano)
