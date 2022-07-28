fbb416939d8d97c640d8a924ba29ab73376e58d6
Dimas Capelari <dimas.capelari@gmail.com>
2022-07-28 15:20:14 -0300

    Desafio

--- TypeScript-Udemy/ecmascript/ecmascript.js
+++ TypeScript-Udemy/ecmascript/ecmascript.js
@@ -137,4 +137,33 @@ Boas vindas ${usuarioID},
 Notificações: ${parseInt(notificacoes) > 9 ? '+9' : notificacoes}
 `;
 console.log(boasVindas);
+// Desafios
+// Exercicio 1
+const dobro = (valor) => valor * 2;
+console.log(dobro(10));
+// Exercicio 2
+const dizerOla = function (nome = 'Pessoa') {
+    console.log("Ola, " + nome);
+};
+dizerOla();
+dizerOla("Anna");
+// Exercicio 3
+const nums = [-3, 33, 38, 5];
+// Imprimir o menor valor
+console.log(Math.min(...nums));
+// Exercicio 4
+const array = [55, 20,];
+array.push(...nums);
+// Adicionar todos os elementos de "nums" em array
+console.log(array);
+// Exercicio 5
+const notas = [8.5, 6.3, 9.4];
+const [nota1, nota2, nota3] = notas;
+console.log(nota1, nota2, nota3);
+// Exercicio 6
+const cientista = { primeiroNome: "Will", experiencia: 12 };
+const { primeiroNome, experiencia } = cientista;
+console.log(primeiroNome, experiencia);
+//console.log(cientista.primeiroNome, cientista.experiencia)
+//------------------------------------------------------------------
 //# sourceMappingURL=ecmascript.js.map
--- TypeScript-Udemy/ecmascript/ecmascript.ts
+++ TypeScript-Udemy/ecmascript/ecmascript.ts
@@ -180,3 +180,40 @@ Notificações: ${parseInt(notificacoes) > 9 ? '+9' : notificacoes}
 `
 
 console.log(boasVindas)
+
+
+
+// Desafios
+// Exercicio 1
+const dobro = (valor: number): number => valor * 2
+console.log(dobro(10))
+
+// Exercicio 2
+const dizerOla = function (nome: string = 'Pessoa'): void {
+    console.log("Ola, " + nome)
+}
+dizerOla()
+dizerOla("Anna")
+
+// Exercicio 3
+const nums = [-3, 33, 38, 5]
+// Imprimir o menor valor
+console.log(Math.min(...nums))
+
+// Exercicio 4
+const array = [55, 20,]
+array.push(...nums)
+// Adicionar todos os elementos de "nums" em array
+console.log(array)
+
+// Exercicio 5
+const notas = [8.5, 6.3, 9.4]
+const [nota1, nota2, nota3] = notas
+console.log(nota1, nota2, nota3)
+
+// Exercicio 6
+const cientista = { primeiroNome: "Will", experiencia: 12 }
+const { primeiroNome, experiencia } = cientista
+console.log(primeiroNome, experiencia)
+//console.log(cientista.primeiroNome, cientista.experiencia)
+//------------------------------------------------------------------
