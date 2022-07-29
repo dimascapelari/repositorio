3da0ff5207edf6189438050487dbbd530a8935fa
Dimas Capelari <dimas.capelari@gmail.com>
2022-07-29 11:43:18 -0300

    TypeScript-VueJS

---
+++ TypeScript-Udemy/TypeScript-VueJS/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <CompContador :valorInicial="10" />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import CompContador from "./components/CompContador.vue";
+
+@Component({
+  components: {
+    CompContador,
+  },
+})
+export default class App extends Vue {}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ TypeScript-Udemy/TypeScript-VueJS/src/components/CompContador.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h1>Contador</h1>
+    <h2>{{ valor }}</h2>
+    <button @click="setValor(-1)">Dec</button>
+    <button @click="setValor(1)">Inc</button>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  props: {
+    valorInicial: {
+      type: Number,
+    },
+  },
+
+  data() {
+    return {
+      valor: this.valorInicial || 0,
+    };
+  },
+  methods: {
+    setValor(delta: number) {
+      this.valor += delta;
+    },
+  },
+});
+</script>
+
+<style>
+</style>
