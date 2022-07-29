e6df1364f39ae0816531ceb9ffb9fb34126364d9
Dimas Capelari <dimas.capelari@gmail.com>
2022-07-28 21:33:52 -0300

    Desafio Classe Produto

--- TypeScript-Udemy/classes/classes.js
+++ TypeScript-Udemy/classes/classes.js
@@ -13,4 +13,36 @@ console.log(aniversario);
 const casamento = new Data; // posso omitir os "()"
 casamento.ano = 2017;
 console.log(casamento);
+// ----------------------------------------------------------
+class DataEsperta {
+    constructor(dia = 1, mes = 1, ano = 1970) {
+        this.dia = dia;
+        this.mes = mes;
+        this.ano = ano;
+    }
+}
+const aniversarioEsperto = new DataEsperta(3, 11, 1991);
+aniversarioEsperto.dia = 4;
+console.log(aniversarioEsperto.dia);
+console.log(aniversarioEsperto);
+const casamentoEsperto = new DataEsperta; // posso omitir os "()"
+casamentoEsperto.ano = 2017;
+console.log(casamentoEsperto);
+// Desafio Classe Produto
+// Atributos: nome, preco e desconto
+// Criar o construtor
+// Obs 1.: Desconto é opcional (valor padrão 0)
+// Obs 2.: Criar dois produtos: passando dois e três params
+class Produto {
+    constructor(nome, preco, desconto = 0) {
+        this.nome = nome;
+        this.preco = preco;
+        this.desconto = desconto;
+    }
+}
+const prod1 = new Produto('Caneta Bic Preta', 4.20);
+prod1.desconto = 0.06;
+console.log(prod1);
+const prod2 = new Produto('Caderno Escolar', 18.80, 0.15);
+console.log(prod2);
 //# sourceMappingURL=classes.js.map
--- TypeScript-Udemy/classes/classes.ts
+++ TypeScript-Udemy/classes/classes.ts
@@ -23,3 +23,47 @@ console.log(aniversario)
 const casamento = new Data // posso omitir os "()"
 casamento.ano = 2017
 console.log(casamento)
+
+// ----------------------------------------------------------
+
+class DataEsperta {
+
+    constructor(
+        public dia: number = 1,
+        public mes: number = 1,
+        public ano: number = 1970) {
+    }
+}
+
+const aniversarioEsperto = new DataEsperta(3, 11, 1991)
+aniversarioEsperto.dia = 4
+console.log(aniversarioEsperto.dia)
+console.log(aniversarioEsperto)
+
+const casamentoEsperto = new DataEsperta // posso omitir os "()"
+casamentoEsperto.ano = 2017
+console.log(casamentoEsperto)
+
+
+// Desafio Classe Produto
+// Atributos: nome, preco e desconto
+// Criar o construtor
+// Obs 1.: Desconto é opcional (valor padrão 0)
+// Obs 2.: Criar dois produtos: passando dois e três params
+
+class Produto {
+    constructor(
+        public nome: string,
+        public preco: number,
+        public desconto: number = 0
+    ) {
+
+    }
+}
+
+const prod1 = new Produto('Caneta Bic Preta', 4.20)
+prod1.desconto = 0.06
+console.log(prod1)
+
+const prod2 = new Produto('Caderno Escolar', 18.80, 0.15)
+console.log(prod2)
