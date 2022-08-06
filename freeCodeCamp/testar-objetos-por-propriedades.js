677fcf8ecb911ff8685d7ae533dbf3f7528d51e9
Dimas Capelari <dimas.capelari@gmail.com>
2022-08-06 15:36:17 -0300

    Create testar-objetos-por-propriedades.js

---
+++ freeCodeCamp/testar-objetos-por-propriedades.js
@@ -0,0 +1,12 @@
+function checkObj(obj, checkProp) {
+  // Altere apenas o código abaixo desta linha
+
+if (obj.hasOwnProperty(checkProp)) {
+    return obj[checkProp];
+  } else {
+    return "Not Found";
+  }
+
+  // Altere apenas o código acima desta linha
+}
+
