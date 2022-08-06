63f0fa167dc92f9b1c7aa629afbe43585691b813
Dimas Capelari <dimas.capelari@gmail.com>
2022-08-06 15:33:23 -0300

    Create pesquisar-perfil.js

---
+++ freeCodeCamp/pesquisar-perfil.js
@@ -0,0 +1,48 @@
+// Configuração
+const contacts = [
+  {
+    firstName: "Akira",
+    lastName: "Laine",
+    number: "0543236543",
+    likes: ["Pizza", "Coding", "Brownie Points"],
+  },
+  {
+    firstName: "Harry",
+    lastName: "Potter",
+    number: "0994372684",
+    likes: ["Hogwarts", "Magic", "Hagrid"],
+  },
+  {
+    firstName: "Sherlock",
+    lastName: "Holmes",
+    number: "0487345643",
+    likes: ["Intriguing Cases", "Violin"],
+  },
+  {
+    firstName: "Kristian",
+    lastName: "Vos",
+    number: "unknown",
+    likes: ["JavaScript", "Gaming", "Foxes"],
+  },
+];
+
+function lookUpProfile(name, prop) {
+  // Altere apenas o código abaixo desta linha
+
+for (let i = 0; i < contacts.length; i++) {
+    if (contacts[i].firstName === name) {
+      if (prop in contacts[i]) {
+        return contacts[i][prop];
+      } else {
+        return "No such property";
+      }
+    }
+  }
+  return "No such contact";
+
+
+  // Altere apenas o código acima desta linha
+}
+
+lookUpProfile("Akira", "likes");
+
