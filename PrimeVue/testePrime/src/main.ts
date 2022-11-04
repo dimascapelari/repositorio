aa969c09ce98381283f5a4ac8ce407dff7deb098
Dimas Capelari <dimas.capelari@gmail.com>
2022-11-04 09:38:44 -0300

    Update main.ts

--- PrimeVue/testePrime/src/main.ts
+++ PrimeVue/testePrime/src/main.ts
@@ -2,7 +2,7 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import PrimeVue from 'primevue/config';
 import router from './router';
-// import Carousel from "primevue/carousel";
+
 
 const app = createApp(App);
 
