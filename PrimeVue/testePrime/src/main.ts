97e1f2a512b85554e9de30078db4ad89b8eb79c7
Dimas Capelari <dimas.capelari@gmail.com>
2022-11-04 09:42:37 -0300

    Update main.ts

--- PrimeVue/testePrime/src/main.ts
+++ PrimeVue/testePrime/src/main.ts
@@ -8,6 +8,7 @@ const app = createApp(App);
 app.use(PrimeVue);
 app.use(router).mount('#app')
 
+
 import 'primevue/resources/themes/saga-blue/theme.css' //theme
 import 'primevue/resources/primevue.min.css'           //core css
 import 'primeicons/primeicons.css' 
