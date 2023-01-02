e13fdef571945f8b8b0b1116fbbef0c5b6456980
Dimas Capelari <97951537+dimascapelari@users.noreply.github.com>
2023-01-02 08:55:46 -0300

    Update console.js

--- Fetch API/console.js
+++ Fetch API/console.js
@@ -2,10 +2,9 @@ function teste() {
   let teste2 = "teste2";
   console.log("teste2");
 }
-
 teste();
 
-
-function testeDois(){
-  alert("Feliz 2023")
+function testeDois() {
+  alert("Feliz 2023");
 }
+testeDois();
