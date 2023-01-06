050d6150d9b0154aae1e2b819d7f9cea978df15b
Dimas Capelari <97951537+dimascapelari@users.noreply.github.com>
2023-01-06 17:10:40 -0300

    Update console.js

--- Fetch API/console.js
+++ Fetch API/console.js
@@ -1,3 +1,4 @@
+/*
 function teste() {
   let teste2 = "teste2";
   console.log("teste2");
@@ -5,6 +6,24 @@ function teste() {
 teste();
 
 function testeDois() {
-  alert("Feliz 2023");
+  console.log("Feliz 2023");
 }
 testeDois();
+*/
+
+// -------------------------------------
+var a = 5;
+var b = "2";
+console.log(a + b);
+console.log(a - b);
+// -------------------------------------
+let x = 10;
+if (1 < x < 3) {
+  console.log("Successful");
+} else {
+  console.log("Unsuccessful");
+}
+// -------------------------------------
+const arr = [5, 18, 25];
+arr.sort();
+console.log(arr[1]);
