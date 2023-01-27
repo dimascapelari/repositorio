551acbd32d4548f5e8dc701a65698733196cbd10
Dimas Capelari <97951537+dimascapelari@users.noreply.github.com>
2023-01-27 09:47:09 -0300

    Update script.js

--- Fetch API/RogerMelo/script.js
+++ Fetch API/RogerMelo/script.js
@@ -9,3 +9,23 @@ const fetchPokemon = () => {
 }
 
 fetchPokemon()
+
+------------------------------------
+
+const listaProdutos = () => {
+  const url = `https://mks-challenge-api-frontend.herokuapp.com/api/v1/products?page=1&rows=6&sortBy=id&orderBy=DESC`;
+
+  fetch(url)
+    .then((response) => response.json())
+    .then((lista) => {
+      console.log(lista);
+      console.log(lista.products);
+      console.log("name: ", lista.products[0].name);
+      console.log("brand: ", lista.products[0].brand);
+      console.log("description: ", lista.products[0].description);
+      console.log("photo: ", lista.products[0].photo);
+    });
+};
+
+listaProdutos();
+
